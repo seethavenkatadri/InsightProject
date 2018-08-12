1961d6c510ca2ded5d44a1ef78439cef77d81a08
Seetha Venkatadri <seethavenkatadri@gmail.com>
2018-08-11 17:12:54 -0700

    Moving plane with styled map

--- app/app.py
+++ app/app.py
@@ -1,23 +1,13 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='/static')
 app.config['DEBUG'] = True
-POSTGRES = {
-    'user': 'postgres',
-    'pw': 'postgres',
-    'db': 'DB_MAIN_SCHEMA',
-    'host': 'ec2-54-203-148-178.us-west-2.compute.amazonaws.com',
-    'port': '5432',
-}
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
-%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-
 
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('airtravel.html')
 
 if __name__ == '__main__':
     app.run()
