e1bddae360c8ef1ff64289f9fc3aaacefbe7bbe9
Seetha Venkatadri <seethavenkatadri@gmail.com>
2018-08-12 20:57:04 -0700

    Changing input from list to dict to get header; renaming template title.

--- ingestion/streaming.py
+++ ingestion/streaming.py
@@ -20,7 +20,7 @@ def get_all_bucket_files(my_bucket):
 
     fileHandleList=[]
     ## getting all files in csv folder
-    for object in my_bucket.objects.filter(Prefix='csv/'):
+    for object in my_bucket.objects.filter(Prefix='sample_data/'):
         fileHandleList.append(get_file_handle(my_bucket, object.key))
     return fileHandleList
 
@@ -38,7 +38,7 @@ def publish_message(producerInstance, topic_name, value):
 def connect_kafka_producer():
     "Function to create a producer handle"
     _producer = None
-    conf = {'bootstrap.servers': 'ec2-34-210-24-198.us-west-2.compute.amazonaws.com:9092'}
+    conf = {'bootstrap.servers': 'ec2-54-203-179-203.us-west-2.compute.amazonaws.com:9092'}
     try:
         _producer = confluent_kafka.Producer(conf)
     except Exception as ex:
@@ -52,8 +52,16 @@ if __name__ == '__main__':
     ##Limited number of records as of now
     num_records=0
     num_files=0
+    flight_record=["icao24", "callsign", "origin_country", "time_position", "last_contact", "longitude", "latitude", "geo_altitude",
+     "on_ground", "velocity", "true_track", "vertical_rate", "sensors", "baro_altitude", "squawk", "spi",
+     "position_source"]
+    weather_record=["ID","USAF","WBAN","Elevation","Country_Code","Latitude","Longitude","Date","Year","Month","Day","Mean_Temp","Mean_Temp_Count","Mean_Dewpoint",
+                    "Mean_Dewpoint_Count","Mean_Sea_Level_Pressure","Mean_Sea_Level_Pressure_Count","Mean_Station_Pressure","Mean_Station_Pressure_Count",
+                    "Mean_Visibility","Mean_Visibility_Count","Mean_Windspeed","Mean_Windspeed_Count","Max_Windspeed","Max_Gust","Max_Temp","Max_Temp_Quality_Flag",
+                    "Min_Temp","Min_Temp_Quality_Flag","Precipitation","Precip_Flag	Snow_Depth","Fog","Rain_or_Drizzle","Snow_or_Ice","Hail","Thunder","Tornado"]
+
     ############   Main Function   ###############
-    bucketName = sys.argv[1]
+    bucketName = sys.arg[1]
     topicName = sys.argv[2]
     myBucket=get_bucket_details(bucketName)
 
@@ -63,9 +71,15 @@ if __name__ == '__main__':
 
     #for file in fileHandleList:
     for file in fileHandleList:
-        for record in csv.DictReader(codecs.getreader('utf-8')(file.get()[u'Body'])):
-            print(record)
-            publish_message(kafkaProducer, topicName, json.dumps(record))
+        for record in codecs.getreader('utf-8')(file.get()[u'Body']):
+            arr=record.strip().split(',')
+            if topicName == 'topic-flight':
+                resultDict = dict({flight_record[i]:arr[i] for i in range(len(arr))})
+            else:
+                resultDict = dict({weather_record[i]: arr[i] for i in range(len(arr))})
+            print(resultDict)
+
+            publish_message(kafkaProducer, topicName, json.dumps(resultDict))
             num_records+=1
             if num_records == 10:
                 break
