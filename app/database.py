0790c89757d828438837dfdb4b628e79956598ab
Seetha Venkatadri <seethavenkatadri@gmail.com>
2018-08-19 15:06:42 -0700

    Fetch json from postgres

--- app/database.py
+++ app/database.py
@@ -1,5 +1,50 @@
-from flask_sqlalchemy import SQLAlchemy
-import app.app
+import json
+import psycopg2
+from configparser import ConfigParser
 
-db = SQLAlchemy()
-db.init_app(app)
+def config(filename='database-fe.ini', section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+def fetch(limit):
+    """ insert a new record into the flying conditions table """
+    sql = "select * from flight limit %s";
+    conn = None
+    state_id = None
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql, (limit,))
+        r = [dict((cur.description[i][0], value) \
+                  for i, value in enumerate(row)) for row in cur.fetchall()]
+        cur.connection.close()
+        return (r[0] if r else None)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+my_query = fetch(3)
+
+json_output = json.dumps(my_query)
+print(json_output)
